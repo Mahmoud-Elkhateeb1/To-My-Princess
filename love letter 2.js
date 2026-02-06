13be8ae48b6a13942d10798408518375946f6694
Mahmoud-Elkhateeb1 <mahmoud.elkhateeb@cgtech.com.sa>
2026-02-06 06:10:05 +0200

    first commit

--- love letter 2.js
+++ love letter 2.js
@@ -1,22 +1,26 @@
-$(document).ready(function () {
-  var envelope = $("#envelope");
-  var btn_open = $("#open");
-  var btn_reset = $("#reset");
+$(document).ready(function() {
+    var envelope = $("#envelope");
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    var audio = document.getElementById("bgm");
 
-  envelope.click(function () {
-    open();
-  });
-  btn_open.click(function () {
-    open();
-  });
-  btn_reset.click(function () {
-    close();
-  });
+    envelope.click(function() {
+        open();
+    });
+    btn_open.click(function() {
+        open();
+    });
+    btn_reset.click(function() {
+        close();
+    });
 
-  function open() {
-    envelope.addClass("open").removeClass("close");
-  }
-  function close() {
-    envelope.addClass("close").removeClass("open");
-  }
+    function open() {
+        envelope.addClass("open").removeClass("close");
+        audio.play();
+    }
+
+    function close() {
+        envelope.addClass("close").removeClass("open");
+        // audio.pause();
+    }
 });
